7eb08235d0f52ab74d47c7e4c07a9bd7f183dad3
lbs <lbspen@gmail.com>
2013-05-20 15:04:48 -0700

    Generated initial app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudioSchedule::Application.config.secret_token = '407684bb87d0f1c0c5720858e41a748aeeec408a79bcdcdb588dcd10ff331ea181a0115035c0c851b5862b44ed059d89e3bff38fa09460f0f1c97aa2e897a04a'
