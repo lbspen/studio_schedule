5048b929fe8c8da04ea5fa9f5ca0c99f408b98c8
peter <peter@peter-HP-Compaq-6730b-KS181UT-ABA.(none)>
2013-06-21 00:49:26 -0700

    added datepicker, timepicker, fullcalendar jquery plugins.  Need to fix date format produced by selector, hook up calendar to google cal?

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,22 @@
 //
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
-//
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.all
+//= require jquery.timepicker.js
+//= require datepair.js
+//= require fullcalendar
+//= require gcal
 //= require foundation
 //= require_tree .
 
 $(document).foundation();
+
+$(function() {
+  $( "#datepicker" ).datepicker();
+  $('.pick-time').timepicker();
+  $('#calendar').fullCalendar({
+    defaultView: 'agendaWeek'
+  });
+});
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -2,7 +2,7 @@ class Timeslot < ActiveRecord::Base
   attr_accessible :end_time, :recording_session_date, :start_time
 
   validates_date :recording_session_date, :on_or_after => DateTime.now
-  
+
   def date_to_s
     recording_session_date.strftime("%B %-d, %Y")
   end
